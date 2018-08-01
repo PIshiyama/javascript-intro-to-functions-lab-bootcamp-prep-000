7e924c4fc93458ca9327247a094803e2518ec928
Paul Ishiyama <paul.h.ishiyama@gmail.com>
2018-08-01 22:31:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var string = 'hello'
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(shout(string))
+}
+
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+
+function sayHiToGrandma(string) {
+  
+}
