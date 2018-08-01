dae7be3935ec037786cbcc3c540789d90f906789
Paul Ishiyama <paul.h.ishiyama@gmail.com>
2018-08-01 23:25:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function logWhisper(string) {
   console.log(whisper(string))
 }
 
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!";
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
